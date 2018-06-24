02195b04028b0dd3c9cf892e8860b8d0dd91438c
zerg1996 <zerg1996@yandex.ru>
2018-06-24 22:23:46 +0300

    Apply patch and add header prefix

--- libs/config/test/fail.cpp
+++ libs/config/test/fail.cpp
@@ -4,17 +4,15 @@
 
 #include <boost/config.hpp>
 #include "test.hpp"
-
 @BOOST_CONFIG_TR1_INCLUDE@
 
 @BOOST_CONFIG_IFDEF@ @BOOST_CONFIG_MACRO@
-
 #include "@BOOST_CONFIG_FILE@.ipp"
-
 #else
 #error "this file should not compile"
 #endif
 
-int main(int, char *[]) {
-    return @BOOST_CONFIG_NS@::test();
+int main( int, char *[] )
+{
+   return @BOOST_CONFIG_NS@::test();
 }
