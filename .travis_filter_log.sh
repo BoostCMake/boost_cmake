46569ea5a81560084b991f4944786f75f68d090d
Zerg1996 <zerg1996@yandex.ru>
2018-11-08 23:31:41 +0300

    Add filter for travis log output

---
+++ .travis_filter_log.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+TIME_START=$(date +%s)
+STEP=5
+WAS_OUTPUT_STEP=false
+WAS_OUTPUT_300=false
+echo ${START}
+
+while read line; do
+  TIME_CURRENT=$(date +%s)
+  #echo $(((${TIME_CURRENT} - ${TIME_START}) % ${STEP}))
+  if [ "$WAS_OUTPUT_STEP" = false ] ; then
+      echo "${line}"
+      echo ${TIME_CURRENT}
+      WAS_OUTPUT_STEP=true
+  fi
+  if [ "WAS_OUTPUT_300" = false ] ; then
+      echo "Make in progress"
+      WAS_OUTPUT_300=true
+  fi
+  if [ $(((${TIME_CURRENT} - ${TIME_START}) % ${STEP})) == 0 ] ; then
+     WAS_OUTPUT_STEP=false
+  fi 
+  if [ $(((${TIME_CURRENT} - ${TIME_START}) % ${STEP})) == 300 ] ; then
+     WAS_OUTPUT_300=false
+  fi 
+done < /dev/stdin
